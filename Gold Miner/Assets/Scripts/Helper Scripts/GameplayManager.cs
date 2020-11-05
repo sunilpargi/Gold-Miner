1532464da707dec6156891591f12164102b73a69
Sunil pargi <sunilpargi121@gmail.com>
2020-11-05 09:07:27 +0530

    added hook movement

---
+++ Gold Miner/Assets/Scripts/Helper Scripts/GameplayManager.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameplayManager : MonoBehaviour {
+
+    public static GameplayManager instance;
+
+    [SerializeField]
+    private Text countdownText;
+
+    public int countdownTimer = 60;
+
+    [SerializeField]
+    private Text scoreText;
+
+    private int scoreCount;
+
+    [SerializeField]
+    private Image scoreFillUI;
+
+    void Awake() {
+        if (instance == null)
+            instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start() {
+
+        DisplayScore(0);
+
+        countdownText.text = countdownTimer.ToString();
+
+        StartCoroutine("Countdown");
+
+    }
+
+    IEnumerator Countdown() {
+        yield return new WaitForSeconds(1f);
+
+        countdownTimer -= 1;
+
+        countdownText.text = countdownTimer.ToString();
+
+        if(countdownTimer <= 10) {
+            SoundManager.instance.TimeRunningOut(true);
+        }
+
+        StartCoroutine("Countdown");
+
+        if(countdownTimer <= 0) {
+            StopCoroutine("Countdown");
+
+            SoundManager.instance.GameEnd();
+            SoundManager.instance.TimeRunningOut(false);
+
+            StartCoroutine(RestartGame());
+        }
+
+    } // countdown
+
+    public void DisplayScore(int scoreValue) {
+
+        if (scoreText == null)
+            return;
+
+        scoreCount += scoreValue;
+        scoreText.text = "$ " + scoreCount;
+
+        scoreFillUI.fillAmount = (float)scoreCount / 100f;
+
+        if(scoreCount >= 100) {
+            StopCoroutine("Countdown");
+            SoundManager.instance.GameEnd();
+            StartCoroutine(RestartGame());
+        }
+
+    }
+
+    IEnumerator RestartGame() {
+        yield return new WaitForSeconds(4f);
+
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Gameplay");
+    }
+
+} // class
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
